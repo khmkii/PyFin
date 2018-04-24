c6b9cc5c7703d87c4b66613bfd3f2b466fc6343f
khmkii <james_mccombie@hotmail.com>
2018-04-24 20:57:00 +0100

    initial commit branch version_1

--- BinomialInterestRateTree/bin_int_tests.py
+++ BinomialInterestRateTree/bin_int_tests.py
@@ -6,42 +6,7 @@ from binomial_tree import BinomialTree, Node
 
 
 class BinomialTreeTests(unittest.TestCase):
-
-    def setUp(self):
-        self.rates = [1, 1.402, 1.351, 1.864, 3.5]
-        self.volatility = random.randint(5, 20)
-
-    def tearDown(self):
-        del self.rates
-        del self.volatility
-
-    def test_binomial_tree_lower_population(self):
-        btree = BinomialTree(self.volatility, self.rates)
-        calculated_rates = [
-            ra * (math.e ** (-1 * (en + 1) * (self.volatility / 100) ) ) for
-            en, ra in enumerate(self.rates[1:])
-        ]
-        calculated_rates.insert(0, self.rates[0])
-        binomial_rates = [btree.root_node.value]
-        next = btree.root_node.down_child
-        while next is not None:
-            binomial_rates.append(next.value)
-            next = next.down_child
-        self.assertEqual(binomial_rates, calculated_rates)
-
-    def test_binomial_tree_upper_population(self):
-        btree = BinomialTree(self.volatility, self.rates)
-        calculated_rates = [
-            ra * (math.e ** (1 * (en + 1) * (self.volatility / 100) ) ) for
-            en, ra in enumerate(self.rates[1:])
-        ]
-        calculated_rates.insert(0, self.rates[0])
-        binomial_rates = [btree.root_node.value]
-        next = btree.root_node.up_child
-        while next is not None:
-            binomial_rates.append(next.value)
-            next = next.up_child
-        self.assertEqual(binomial_rates, calculated_rates)
+    pass
 
 
 if __name__ == '__main__':
--- BinomialInterestRateTree/binomial_tree.py
+++ BinomialInterestRateTree/binomial_tree.py
@@ -1,52 +1,7 @@
 import math
-
-
-class Node:
-    def __init__(self, value, up, down, parent, level, root=False):
-        self.value = value
-        self.up_child = up
-        self.down_child = down
-        self.parent = parent
-        self.level = level
-        self.root = root
-
-    def __str__(self):
-        return str(self.value)
+import numpy as np
 
 
 class BinomialTree:
     def __init__(self, volatility, base_rates):
-        self.root_node = Node(
-            level = 0,
-            parent = None,
-            value = base_rates[0],
-            up = None,
-            down = None,
-            root = True,
-        )
-        current = self.root_node
-        for period in range(1, len(base_rates)):
-            current.down_child = Node(
-                level = period,
-                parent = current,
-                value = base_rates[period] * (
-                    math.e ** (-1 * period * volatility * 10 ** -2)
-                ),
-                up = None,
-                down = None,
-                root = False
-            )
-            current = current.down_child
-        current = self.root_node
-        for period in range(1, len(base_rates)):
-            current.up_child = Node(
-                level = period,
-                parent = current,
-                value = base_rates[period] * (
-                    math.e ** (1 * period * volatility * 10 ** -2)
-                ),
-                up = None,
-                down = None,
-                root = False
-            )
-            current = current.up_child
+        
