b24a13b2470ed904773e832a5c7bc7eb847a2da1
khmkii <james_mccombie@hotmail.com>
2018-04-25 07:20:32 +0100

    BinomialTree class representation a matrix of rates, test for initial calibration completed

--- BinomialInterestRateTree/bin_int_tests.py
+++ BinomialInterestRateTree/bin_int_tests.py
@@ -1,12 +1,31 @@
 import math
+import numpy as np
 import random
 import unittest
 
-from binomial_tree import BinomialTree, Node
+from binomial_tree import BinomialTree
 
 
 class BinomialTreeTests(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.initial_test_matrix = np.array([
+            [0.0, 0.0, 8.325],
+            [0.0, 4.69381, 0.0],
+            [2.0, 0.0, 6.167],
+            [0.0, 3.47726022, 0.0],
+            [0.0, 0.0, 4.56862597],
+        ])
+
+    def test_initial_calibration(self):
+        btree = BinomialTree(15, [2, 4.04, 6.167])
+        self.assertTrue(np.allclose(
+                btree.rates_matrix,
+                self.initial_test_matrix,
+                rtol=1e-3
+            )
+        )
+
 
 
 if __name__ == '__main__':
--- BinomialInterestRateTree/binomial_tree.py
+++ BinomialInterestRateTree/binomial_tree.py
@@ -1,7 +1,31 @@
-import math
+import logging
 import numpy as np
 
 
 class BinomialTree:
+
+    def recalibrate(self, base_rates=None):
+        if base_rates is None:
+            base_rates = self.base_rates
+        for column in range(0, self.levels):
+            lower_value = base_rates[column] * np.e ** (
+                -1 * column * self.volatility
+            )
+            row = self.center + column
+            self.rates_matrix[row][column] = lower_value
+            if column > 0:
+                multiple = 2
+                row -= multiple
+                while row >= 0:
+                    value = lower_value * np.e ** (multiple * self.volatility)
+                    self.rates_matrix[row][column] = value
+                    row -= multiple
+                    multiple += 2
+
     def __init__(self, volatility, base_rates):
-        
+        self.volatility = volatility / 100
+        self.base_rates = base_rates
+        self.levels = len(base_rates)
+        self.center = self.levels - 1
+        self.rates_matrix = np.zeros(shape=((self.levels * 2) - 1, self.levels))
+        self.recalibrate()
